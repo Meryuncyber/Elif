417320848593887d7d668f3af08d8bcff39eb6ee
Meryuncyber <taymuryunus77@gmail.com>
2025-06-29 19:32:43 +0300

    Update exif-reader.js

--- scripts/exif-reader.js
+++ scripts/exif-reader.js
@@ -1,12 +1,12 @@
 // EXIF verilerini oku ve göster
-export function readExifData(file, callback) {
-  const reader = new FileReader();
+function readExifData(file, callback) {
+  var reader = new FileReader();
 
   reader.onload = function (event) {
-    const img = new Image();
+    var img = new Image();
     img.onload = function () {
       EXIF.getData(img, function () {
-        const exifData = EXIF.getAllTags(this);
+        var exifData = EXIF.getAllTags(this);
         callback(exifData);
       });
     };
@@ -17,14 +17,18 @@ export function readExifData(file, callback) {
 }
 
 // EXIF verilerini yazıya çevir
-export function formatExifData(data) {
+function formatExifData(data) {
   if (!data || Object.keys(data).length === 0) {
     return 'Bu dosyada EXIF verisi bulunamadı.';
   }
 
-  let output = '';
-  for (const key in data) {
-    output += `${key}: ${data[key]}\n`;
+  var output = '';
+  for (var key in data) {
+    output += key + ': ' + data[key] + '\n';
   }
   return output;
 }
+
+// Global olarak kullanılabilir hale getir
+window.readExifData = readExifData;
+window.formatExifData = formatExifData;
